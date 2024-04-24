4e3ba424d4c46e98f8586af6bb243ed448f4220f
Alexis Soto <asoto78@gmail.com>
2024-04-24 08:35:30 -0500

    4/24/24

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -6,14 +6,19 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: . leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack{
+                Text("Joshua tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+                
+            }
         }
         .padding()
     }
